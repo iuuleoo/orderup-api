fddba2aded419521c3d07f91645fb35e84384eb1
Leo <leooo0s2@icloud.com>
2026-01-13 15:05:59 -0300

    Adicionando produto no banco

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
-import { ZodError, z } from "zod";
+import { knex } from "@/database/knex";
+import { z } from "zod";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
@@ -20,7 +21,9 @@ class ProductController {
 
       const { name, price } = bodySchema.parse(request.body);
 
-      return response.status(201).json({ name, price });
+      await knex<ProductRepository>("products").insert({ name, price });
+
+      return response.status(201).json();
     } catch (error) {
       next(error);
     }
