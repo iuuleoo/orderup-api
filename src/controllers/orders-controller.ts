0f075aa1622385259a0af8d3751cc598a48ee40c
Leo <leooo0s2@icloud.com>
2026-01-14 22:40:08 -0300

    Listando pedidos por mesa

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -4,7 +4,7 @@ import { knex } from "@/database/knex";
 import { z } from "zod";
 
 class OrdersController {
-  async create(request: Request, response: Response, next: NextFunction) {
+  create = async (request: Request, response: Response, next: NextFunction) => {
     try {
       const bodySchema = z.object({
         table_session_id: z.number(),
@@ -47,11 +47,25 @@ class OrdersController {
     } catch (error) {
       next(error);
     }
-  }
+  };
 
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json();
+      const { table_session_id } = request.params;
+
+      const order = await knex("orders")
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "products.name",
+          "orders.price",
+          "orders.quantity"
+        )
+        .join("products", "products.id", "orders.product_id")
+        .where({ table_session_id });
+
+      return response.json(order);
     } catch (error) {
       next(error);
     }
